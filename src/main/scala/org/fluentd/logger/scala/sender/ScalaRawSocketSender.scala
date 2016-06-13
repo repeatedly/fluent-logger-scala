29f6fa0160e9aae2c85b15a2cc23c7cfa5fc08d3
Homma Takamasa <homma_takamasa@cyberagent.co.jp>
2016-06-13 18:57:10 +0900

    Fix to return false if it exceeds the buffer capacity.

--- src/main/scala/org/fluentd/logger/scala/sender/ScalaRawSocketSender.scala
+++ src/main/scala/org/fluentd/logger/scala/sender/ScalaRawSocketSender.scala
@@ -110,8 +110,7 @@ class ScalaRawSocketSender(h:String, p:Int, to:Int, bufCap:Int)
     try {
       // serialize tag, timestamp and data
       val json = Serialization.write(event)
-      send(json.getBytes("UTF-8"))
-      return true
+      return send(json.getBytes("UTF-8"))
     } catch {
       case e: IOException =>
         LOG.severe("Cannot serialize event: " + event);
